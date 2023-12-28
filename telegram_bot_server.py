a2a9f1c771949bb42c9c93f70c5ff215374dce16
Ruben Kostandyan <kostard@amazon.com>
2023-12-28 17:39:05 +0100

    Adding exception handling, converting to mp3, improving logging

--- telegram_bot_server.py
+++ telegram_bot_server.py
@@ -24,6 +24,19 @@ client = OpenAI()
 ALLOWED_USERS = json.loads(os.getenv("ALLOWED_USERS"))
 
 
+def with_exception_replying(message):
+    def inner(func):
+        async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+            try:
+                await func(update, context)
+            except Exception as e:
+                logging.error(e)
+                await update.effective_message.reply_text(message, quote=True)
+
+        return wrapper
+    return inner
+
+
 async def authenticate(update: Update):
     if update.effective_user.id not in ALLOWED_USERS:
         await update.effective_message.reply_text(get_access_denied_message())
@@ -42,12 +55,14 @@ async def handle_help_command(update: Update, context: ContextTypes.DEFAULT_TYPE
     await update.message.reply_text(response_message)
 
 
+@with_exception_replying("Something went wrong with the processing of this file")
 async def transcribe_voice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await authenticate(update)
     file = await update.message.voice.get_file()
     await _handle_audio_file(update, file)
 
 
+@with_exception_replying("Something went wrong with the processing of this file")
 async def transcribe_audio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await authenticate(update)
     file = await update.message.audio.get_file()
@@ -56,8 +71,8 @@ async def transcribe_audio(update: Update, context: ContextTypes.DEFAULT_TYPE) -
 
 async def _handle_audio_file(update, file):
     """
-    We are converting the received audio file to wav file, as sometimes some audio files are not accepted by whisper,
-    and it works better after converting to wav. This adds some latency, so we can tackle to receive some optimization.
+    We are converting the received audio file to mp3 file, as sometimes some audio files are not accepted by whisper,
+    and it works better after converting to mp3. This adds some latency, so we can tackle to receive some optimization.
     """
     file_format = file.file_path.split(".")[-1]
     path = await file.download_to_drive(f"/tmp/{uuid.uuid4()}.{file_format}")
@@ -65,16 +80,18 @@ async def _handle_audio_file(update, file):
         converted_audio = _convert_audio(downloaded_file, file_format)
         transcript = client.audio.transcriptions.create(
             model="whisper-1",
-            file=("test.wav", converted_audio.read())
+            file=("test.mp3", converted_audio.read())
         )
         await update.effective_message.reply_text(transcript.text)
 
 
 def _convert_audio(input_file, file_format) -> io.BytesIO:
-    wav_io = io.BytesIO()
-    AudioSegment.from_file(input_file, type=file_format).export(wav_io, format="wav")
-    wav_io.seek(0)
-    return wav_io
+    logging.info("Converting the audio file")
+    mp3_io = io.BytesIO()
+    AudioSegment.from_file(input_file, type=file_format).export(mp3_io, format="mp3", bitrate="64")
+    mp3_io.seek(0)
+    logging.info("Done converting the audio file")
+    return mp3_io
 
 
 async def text_message_received(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
